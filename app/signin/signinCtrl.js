d4723234ec780344db16253171a97f09e566c55a
meni bibas <menibibas@gmail.com>
2019-06-25 23:55:10 +0300

    user sign in hardcoded logic

--- app/signin/signinCtrl.js
+++ app/signin/signinCtrl.js
@@ -1,3 +1,14 @@
-app.controller("signinCtrl", function ($scope) {
-    $scope.test = "meni";
+app.controller("signinCtrl", function ($scope, $location) {
+    $scope.invalidCredentials = false;
+    $scope.email = "";
+    $scope.pwd = "";
+
+    $scope.signin = function () {
+        if($scope.email==="menibibas@gmail.com" && $scope.pwd==="1234"){
+            $location.path("/records")
+        } else {
+            $scope.invalidCredentials = true;
+        }
+    }
+    
 })
