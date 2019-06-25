e798c9e2a130e291f13abb98907f6f627ccdf368
meni bibas <menibibas@gmail.com>
2019-06-25 18:20:57 +0300

    setting up routing and homepage jumbotron

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "app/home/home.html"
+        }).when("/signin", {
+
+        }).when("/signup", {
+
+        }).when("/records", {
+
+        }).when("/recordsDetails:id", {
+
+        })
+
+})
+
