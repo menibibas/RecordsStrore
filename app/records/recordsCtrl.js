13f2c2750af16776c99dd26b23e6cf62ab506f58
meni bibas <menibibas@gmail.com>
2019-06-27 17:17:35 +0300

    creating the records gallery html including JSON including designing home page and gallery

--- app/records/recordsCtrl.js
+++ app/records/recordsCtrl.js
@@ -1,3 +1,27 @@
-app.controller("recordsCtrl", function ($scope) {
+app.controller("recordsCtrl", function ($scope, $http) {
+
+    function Record(id, bname, aname, desc, image, price) {
+        this.id = id;
+        this.bname = bname;
+        this.aname = aname;
+        this.desc = desc;
+        this.image = image;
+        this.price = price;
+      }
     
-})
+      $scope.records = [];
+      $http.get("app/Model/data/records.json").then(function(res) {
+        // on success
+        for (var i = 0; i < res.data.length; i++) {
+          var record = new Record(res.data[i].id, res.data[i].bname, res.data[i].aname,
+            res.data[i].desc, res.data[i].image, res.data[i].price);
+            $scope.records.push(record);
+          }
+      }, function(err) {
+        console.error(err);
+    
+      })
+
+
+
+});
