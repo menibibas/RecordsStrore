4c0d8876388b4de0cb0f89e22d18500d2ee50626
meni bibas <menibibas@gmail.com>
2019-07-03 12:02:14 +0300

    updating the checkoutcart with the sum of the shopping cart

--- app/checkoutcart/checkoutcartCtrl.js
+++ app/checkoutcart/checkoutcartCtrl.js
@@ -1,6 +1,8 @@
 app.controller("checkoutcartCtrl", function ($scope, $http, $location, shoppingcartSrv) {
 
-
+    $scope.totalSum=function() {
+        return shoppingcartSrv.totalSum();
+         }
 
 
    
